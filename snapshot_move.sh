c908d33011992ef374c0415e2d5aef7281a293a3
Patrick Hamers <phamers@ziggo.nl>
2021-07-05 13:03:22 +0200

    Updated cron info

--- snapshot_move.sh
+++ snapshot_move.sh
@@ -4,6 +4,10 @@
 # This script moves the snapshot file to the root of the webserver
 # The old snapshot will be available as 'snapshot_old.tar.xz"
 
+# You can create a symbolic link to this script to /etc/cron.daily
+# chmod +x ./snapshot_move.sh
+# ln -s ./snapshot_move.sh /etc/cron.hourly/snapshot_move
+
 # VARS:
 SNAPSHOTSOURCEFILE="/tmp/snapshot.tar.xz"
 SNAPSHOTDESTFILE="/var/www/html/snapshot.tar.xz"
